756b61c417801c35219afc523933fda639955828
rahmatillokh <webcode292@gmail.com>
2023-05-27 21:35:44 +0500

    mongoose

--- index.js
+++ index.js
@@ -2,6 +2,15 @@ import express from "express";
 import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 import { engine, create } from "express-handlebars";
+import mongoose, { MongooseError } from "mongoose";
+import * as dotenv from "dotenv"
+
+// ROUTES
+import AuthRoutes from "./routes/auth.js"
+import ProductRoutes from "./routes/products.js"
+
+
+dotenv.config()
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,14 +24,19 @@ const hbs = create({
 app.engine("hbs", hbs.engine);
 app.set("view engine", "hbs");
 app.set("views", "./views");
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static("public"))
+app.use(express.json())
 
-app.get("/", (req, res) => {
-  res.render("index");
-});
+app.use(AuthRoutes)
+app.use(ProductRoutes)
 
-app.get("/about", (req, res) => {
-  res.render("about");
-});
+
+mongoose.connect("mongodb+srv://webcoder292:sysc2Ct3GnSjctJX@cluster0.cuwacpg.mongodb.net/?retryWrites=true&w=majority", {
+  useNewUrlParser: true,
+}, () => console.log("mongo db connected"));
+
 
 const PORT = process.env.PORT || 4100;
 app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+
